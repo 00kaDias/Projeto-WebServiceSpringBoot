dd2130d37b3033eada28228d95b0d29eef943937
Kauan Victor <victtordiasp1@gmail.com>
2026-09-02 20:35:18 -0300

    User insert and URI

--- src/main/java/com/kadias/demospringboot/services/UserServices.java
+++ src/main/java/com/kadias/demospringboot/services/UserServices.java
@@ -23,4 +23,10 @@ public class UserServices {
         Optional<User> obj = userRepository.findById(id);
         return obj.get();
     }
+
+    public User insert(User obj) {
+        return userRepository.save(obj);
+    }
+
+
 }
